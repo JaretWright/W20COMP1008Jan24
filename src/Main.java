7f8ec8a59a969d5e3ebc4752292e505ac1f5c626
Jaret Wright <hkkA223>
2020-01-24 11:55:08 -0500

    Added start method and minor edits to the fxml

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args)
     {
+        launch(args);
+
         Car car1 = new Car(2019,"Mazda","CX5",18034,"automatic",34754);
         Car car2 = new Car(2019,"Mazda","CX3",12034,"automatic",18763);
         Car car3 = new Car(2020,"Mercedes","C350",2345,"automatic",65473);
@@ -13,5 +21,19 @@ public class Main {
         System.out.printf("The inventory is worth: $%.2f", jamesMotors.getInventoryValue());
         System.out.printf("The cars with model year 2019:%n");
         System.out.printf("%s%n", jamesMotors.getCarsWithYear(2019));
+    }
+
+
+    /**
+     * This method tells the system how to load the FXML file
+     * @param primaryStage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("CardView.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
